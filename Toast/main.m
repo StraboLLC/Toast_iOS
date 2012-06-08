991693cef1311c593f09e0a0b5c00de4c73d225e
Nate Beatty <nate.beatty@gmail.com>
2012-06-08 11:58:43 -0400

    Initial Commit

---
+++ Toast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Toast
+//
+//  Created by Thomas Beatty on 6/8/12.
+//  Copyright (c) 2012 Strabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STRAppDelegate class]));
+    }
+}
